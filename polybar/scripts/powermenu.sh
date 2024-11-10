d4e7c2dfd5adff751317cb9b50ecd12375a28268
Alexxami <150597049+Alexxami@users.noreply.github.com>
2024-11-10 19:23:10 +0100

    Update powermenu.sh
    some errors

--- polybar/scripts/powermenu.sh
+++ polybar/scripts/powermenu.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
-## Author  : Aditya Shakya
-## Mail    : adi1090x@gmail.com
-## Github  : @adi1090x
-## Twitter : @adi1090x
-
-dir="/home/alexx/.config/polybar/rofi"
+dir="~/.config/polybar/rofi"
 uptime=$(uptime -p | sed -e 's/up //g')
 
 rofi_command="rofi -no-config -theme $dir/powermenu.rasi"
